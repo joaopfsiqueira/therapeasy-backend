bba9b93f42c422fc60465ef629eadf0745e28c19
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2024-04-09 08:38:49 -0300

    feat: subindo alterações na tipagem e transformando data-source em var.

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,25 +1,25 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import { Users } from './domain/user/user.entity';
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { Users } from './domain/user/user.entity'
 
 class AppDataSource {
-	DataSource: DataSource;
+    DataSource: DataSource
 
-	constructor() {
-		this.DataSource = new DataSource({
-			type: 'mysql',
-			host: 'localhost',
-			port: 5432,
-			username: 'mysql',
-			password: 'password',
-			database: 'therapy',
-			synchronize: true,
-			logging: false,
-			entities: [Users],
-			migrations: [],
-			subscribers: [],
-		});
-	}
+    constructor() {
+        this.DataSource = new DataSource({
+            type: process.env.DS_TYPE,
+            host: process.env.DS_HOST,
+            port: 3306,
+            username: process.env.DS_USERNAME,
+            password: process.env.DS_PASSWORD,
+            database: process.env.DS_DATABASE,
+            synchronize: true,
+            logging: false,
+            entities: [Users],
+            migrations: [],
+            subscribers: [],
+        })
+    }
 }
 
-export default AppDataSource;
+export default AppDataSource
--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,9 +1,16 @@
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions'
+
 // por conta da propriedade "noPropertyAccessFromIndexSignature": true no tsconfig.ts, eu preciso criar uma interface global do .env, ou isso ou usar process.env.[variável]
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
             readonly PORT: number
             readonly TIMEZONE: string
+            readonly DS_HOST: string
+            readonly DS_TYPE: MysqlConnectionOptions['type'] // tive que tipar o DS_TYPE para ser do mesmo tipo que o type do MysqlConnectionOptions, já que o data-source não aceita o type string.
+            readonly DS_USERNAME: string
+            readonly DS_PASSWORD: string
+            readonly DS_DATABASE: string
         }
     }
 }
