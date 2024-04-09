29b9793db2f3bdabcb1c21a650b384609c0d5f82
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2024-04-09 10:20:47 -0300

    fix: subindo correção de type

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,5 +1,3 @@
-import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions'
-
 // por conta da propriedade "noPropertyAccessFromIndexSignature": true no tsconfig.ts, eu preciso criar uma interface global do .env, ou isso ou usar process.env.[variável]
 declare global {
     namespace NodeJS {
@@ -7,7 +5,6 @@ declare global {
             readonly PORT: number
             readonly TIMEZONE: string
             readonly DS_HOST: string
-            readonly DS_TYPE: MysqlConnectionOptions['type'] // tive que tipar o DS_TYPE para ser do mesmo tipo que o type do MysqlConnectionOptions, já que o data-source não aceita o type string.
             readonly DS_USERNAME: string
             readonly DS_PASSWORD: string
             readonly DS_DATABASE: string
