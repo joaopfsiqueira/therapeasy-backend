f6eb120f1a1e83a290c7adb59f08a656f34f958d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-09-20 19:43:39 -0300

    fix: corrigindo zod

--- src/utils/zod/questions/questionnaire.zod.ts
+++ src/utils/zod/questions/questionnaire.zod.ts
@@ -5,19 +5,19 @@ export const QuestionnaireSchema = z.object({
         required_error: 'Id Person is required',
         invalid_type_error: 'Id Person must be a number',
     }),
-    q_hour: z.string({
+    q_hour: z.date({
         required_error: 'q_hour is required',
         invalid_type_error: 'q_hour must be a Date',
     }),
-    weigth: z.string({
+    weigth: z.number({
         required_error: 'weigth is required',
         invalid_type_error: 'weigth must be a number',
     }),
-    answered: z.string({
+    answered: z.boolean({
         required_error: 'answered is required',
         invalid_type_error: 'answered must be a boolean',
     }),
-    week: z.string({
+    week: z.number({
         required_error: 'week is required',
         invalid_type_error: 'week must be a number',
     }),
