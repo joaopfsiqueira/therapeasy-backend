fa1c4d27084d1a8d5b45de2065eceff16c2e01b7
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-09-23 21:43:19 -0300

    feat: zod

---
+++ src/utils/interfaces/questions/questionTopic/questionTopic.zod.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const questionTopicSchema = z.object({
+    id_question: z.number({
+        required_error: 'id_question is required',
+        invalid_type_error: 'id_question must be a number',
+    }),
+    id_topic: z.number({
+        required_error: 'id_topic is required',
+        invalid_type_error: 'id_topic must be a number',
+    }),
+})
